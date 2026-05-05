36783c85d5dc44555250041b513cd8394d8f89b9
akchdhry <chowdhuryaareb@gmail.com>
2026-05-05 14:54:21 -0400

    new sidebar

--- last-testament-ng/src/app/app.ts
+++ last-testament-ng/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidebarComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('last-testament-ng');
