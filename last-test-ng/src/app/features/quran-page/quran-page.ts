209dc694d1308656c2c79765984c8d3821758e9f
akchdhry <chowdhuryaareb@gmail.com>
2026-05-09 11:53:53 -0400

    quran api (CORS ISSUE)

--- last-test-ng/src/app/features/quran-page/quran-page.ts
+++ last-test-ng/src/app/features/quran-page/quran-page.ts
@@ -1,4 +1,25 @@
 import { Component } from '@angular/core';
+import { environment } from '../../../environments/environment.development';
+import { Language, QuranClient, SearchMode } from "@quranjs/api";
+
+// Initialize the client
+const client = new QuranClient({
+  clientId: environment.QURAN_CLIENT_ID!,
+  clientSecret: environment.QURAN_CLIENT_SECRET!,
+  defaults: {
+    language: Language.ENGLISH,
+  },
+});
+
+const verses = await client.verses.findByRange("1:1", "1:7" , {
+  translations: [20],
+  words: true,
+  translationFields: {
+    languageName: true,
+    resourceName: true,
+    verseKey: true,
+  },
+});
 
 @Component({
   selector: 'app-quran-page',
@@ -6,4 +27,8 @@ import { Component } from '@angular/core';
   templateUrl: './quran-page.html',
   styleUrl: './quran-page.scss',
 })
-export class QuranPage {}
+export class QuranPage {
+
+  verses = verses
+
+}
---
+++ last-test-ng/src/environments/environment.example.ts
@@ -0,0 +1,5 @@
+// example env file
+export const environment = {
+    QURAN_CLIENT_ID: 'clientid',
+    QURAN_CLIENT_SECRET: 'clientsecret',
+};
