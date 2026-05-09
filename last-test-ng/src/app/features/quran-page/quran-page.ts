cc8eb5cec77798214e64ba109f9f6f29177bc56f
akchdhry <chowdhuryaareb@gmail.com>
2026-05-09 12:24:21 -0400

    removed client service from browser

--- last-test-ng/src/app/features/quran-page/quran-page.ts
+++ last-test-ng/src/app/features/quran-page/quran-page.ts
@@ -1,25 +1,4 @@
 import { Component } from '@angular/core';
-import { environment } from '../../../environments/environment.development';
-import { Language, QuranClient, SearchMode } from "@quranjs/api";
-
-// Initialize the client
-const client = new QuranClient({
-  clientId: environment.QURAN_CLIENT_ID!,
-  clientSecret: environment.QURAN_CLIENT_SECRET!,
-  defaults: {
-    language: Language.ENGLISH,
-  },
-});
-
-const verses = await client.verses.findByRange("1:1", "1:7" , {
-  translations: [20],
-  words: true,
-  translationFields: {
-    languageName: true,
-    resourceName: true,
-    verseKey: true,
-  },
-});
 
 @Component({
   selector: 'app-quran-page',
@@ -28,7 +7,4 @@ const verses = await client.verses.findByRange("1:1", "1:7" , {
   styleUrl: './quran-page.scss',
 })
 export class QuranPage {
-
-  verses = verses
-
 }
