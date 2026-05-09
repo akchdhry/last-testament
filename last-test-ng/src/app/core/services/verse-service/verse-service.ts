e7123c15b9a0aadf7e13cd382c7f8205cc22e088
akchdhry <chowdhuryaareb@gmail.com>
2026-05-09 17:59:36 -0400

    created verse service

---
+++ last-test-ng/src/app/core/services/verse-service/verse-service.ts
@@ -0,0 +1,20 @@
+// core/services/verse.service.ts
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Verse } from '@quranjs/api';
+import { environment } from '../../../../environments/environment';
+
+@Injectable({ providedIn: 'root' })
+export class VerseService {
+  private http = inject(HttpClient);
+  private baseUrl = `${environment.apiUrl}/verses`;
+
+  getAll(): Observable<Verse[]> {
+    return this.http.get<Verse[]>(this.baseUrl);
+  }
+
+  getById(id: number): Observable<Verse> {
+    return this.http.get<Verse>(`${this.baseUrl}/${id}`);
+  }
+}
