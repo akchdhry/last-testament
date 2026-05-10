d2a3a1c592047a7162b2b1e0bf66cdbb7efd0f4a
akchdhry <chowdhuryaareb@gmail.com>
2026-05-09 21:01:05 -0400

    added about page to sidebar

--- last-test-ng/src/app/app.routes.ts
+++ last-test-ng/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { QuranPage } from './core/features/quran-page/quran-page';
 import { SunnahPage } from './core/features/sunnah-page/sunnah-page';
 import { AudioPage } from './core/features/audio-page/audio-page';
+import { AboutPage } from './core/features/about-page/about-page';
 
 export const routes: Routes = [
     {path: 'quran', component: QuranPage },
     {path: 'sunnah', component: SunnahPage },
     {path: 'audio', component: AudioPage },
+    {path: 'about', component: AboutPage },
 ];
