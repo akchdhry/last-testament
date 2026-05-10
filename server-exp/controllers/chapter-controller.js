f179097b1fda50e240f45d245063945aadfee78f
akchdhry <chowdhuryaareb@gmail.com>
2026-05-09 22:51:03 -0400

    fixed chapter-controller language issue

--- server-exp/controllers/chapter-controller.js
+++ server-exp/controllers/chapter-controller.js
@@ -1,8 +1,11 @@
+const { Language } = require('@quranjs/api');
 const client = require('../lib/quran-client');
 
 exports.getAll = async (req, res) => {
   try {
-    const chapters = await client.chapters.findAll();
+    const chapters = await client.content.v4.chapters.list({
+      language: Language.ENGLISH
+    });
     res.json(chapters);
   } catch (err) {
     res.status(500).json({ error: 'Failed to fetch chapters' });
@@ -11,7 +14,9 @@ exports.getAll = async (req, res) => {
 
 exports.getById = async (req, res) => {
   try {
-    const chapter = await client.chapters.findById(req.params.id);
+    const chapter = await client.content.v4.chapters.get(req.params.id, {
+      language: Language.ENGLISH
+    });
     res.json(chapter);
   } catch (err) {
     res.status(500).json({ error: 'Failed to fetch chapter: '});
