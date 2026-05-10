44108463688f6f411d631162d298232d8b69f5b3
akchdhry <chowdhuryaareb@gmail.com>
2026-05-10 01:02:49 -0400

    added prophet like Moses to sunnah page

--- last-test-ng/src/app/core/features/sunnah-page/sunnah-page.ts
+++ last-test-ng/src/app/core/features/sunnah-page/sunnah-page.ts
@@ -6,4 +6,43 @@ import { Component } from '@angular/core';
   templateUrl: './sunnah-page.html',
   styleUrl: './sunnah-page.scss',
 })
-export class SunnahPage {}
+export class SunnahPage {
+
+  prophetLikeMoses = `
+
+    There once was a people who believed in and worshipped false gods. \n
+    
+    These people enslaved others and oppressed the lower class, but then a boy was born from among them. He was from a tribe descending directly from Abraham, and he was prophesied to end the oppression of his land, flipping society on its head. \n
+    
+    He was not raised by his biological parents, but he was instead raised in the ruling family of the oppressors. In that family, however, he had a caretaker who treated him extremely well. \n
+    
+    Subsequently, he grew up with high status in his society. Although he lived surrounded by evil, he himself was never oppressed. He also worked in shepherding and later married multiple women. \n
+    
+    After he grew much older, he was made a prophet and messenger by God. He spoke with The Lord “face-to-face” — as a man speaks to a friend (i.e. without mediation). \n
+    
+    He received scripture and divine revelation at The Mountain, and through him God commanded the people to worship Him alone, and to end the oppression. \n
+    
+    However, the tyrants did not agree. They persecuted all those who followed him, and even the family who raised him did not believe in his message from God. \n
+    
+    After years of persecution and oppression under the people, God commanded His people to leave— to make an exodus to a place in which God promised that they could live more comfortably. \n
+    
+    The oppressors came after them when they tried to escape but had no success. The worst of them was killed trying to stop them. \n
+    
+    When they migrated, the man to whom God spoke became the leader and the statesman over His people. He fought battles and wars to protect and defend their land from evil and idolatry. \n
+    
+    Through him, God revealed commandments to His people, so that they would observe the law of God. They were forbidden from eating that which is unclean, and their meat had to be slaughtered in God's name to be permissibly eaten. \n
+    
+    He established a day of the week for worship for God's people. God spoke through him and forbade his nation from doing work at their time of worship. \n
+    
+    He served God's people as a spiritual and political leader since he was their prophet, teacher, and lawgiver. \n 
+    
+    Never again did there arise a prophet like him. \n 
+    
+    After completing his mission as prophet and messenger, he died a peaceful natural death. \n
+    
+    His successors picked up where he left off and conquered the Holy Land, bringing the Israelites back to Jerusalem. \n
+
+    He is Muhammad (ﷺ), the son of Abdullah, the Prophet like Moses.
+  `
+
+}
