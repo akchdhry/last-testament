435f6c6b85c7c73dd5bce522ba22e3d78d59011d
akchdhry <chowdhuryaareb@gmail.com>
2026-05-13 16:03:19 -0400

    added byrange api to verse-service

--- last-test-ng/src/app/core/services/verse-service/verse-service.ts
+++ last-test-ng/src/app/core/services/verse-service/verse-service.ts
@@ -17,4 +17,8 @@ export class VerseService {
   getByKey(key: String): Observable<Verse> {
     return this.http.get<Verse>(`${this.baseUrl}/key/${key}`);
   }
+
+  getByRange(from: String, to: String): Observable<Verse> {
+    return this.http.get<Verse>(`${this.baseUrl}/range?from=${from}&to=${to}`);
+  }
 }
