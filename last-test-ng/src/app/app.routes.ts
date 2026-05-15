46c1317f0ac44a6dfb3682f4ee6e1aa31f2484dc
akchdhry <chowdhuryaareb@gmail.com>
2026-05-15 14:58:17 -0400

    auth-page added

--- last-test-ng/src/app/app.routes.ts
+++ last-test-ng/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { QuranPage } from './core/features/quran-page/quran-page';
 import { SunnahPage } from './core/features/sunnah-page/sunnah-page';
 import { AudioPage } from './core/features/audio-page/audio-page';
 import { AboutPage } from './core/features/about-page/about-page';
+import { AuthPage } from './auth-page/auth-page';
 
 export const routes: Routes = [
     {path: 'quran', component: QuranPage },
     {path: 'sunnah', component: SunnahPage },
     {path: 'audio', component: AudioPage },
     {path: 'about', component: AboutPage },
+    {path: 'oauth/callback', component: AuthPage },
 ];
